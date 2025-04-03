18ab494cb783020093331ef85023c4c206a01016
StijnCSS <stijnzwaard@gmail.com>
2025-04-03 10:43:41 +0200

    bomboclat

--- script.js
+++ script.js
@@ -2,19 +2,30 @@ const canvas = document.getElementById('drawCanvas');
 const ctx = canvas.getContext('2d');
 
 function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  const canvasContainerWidth = window.innerWidth * 0.9;
+  const canvasContainerHeight = window.innerHeight * 0.8;
+  canvas.width = canvasContainerWidth;
+  canvas.height = canvasContainerHeight;
+}
+
+let drawing = false;
+let resetTimeout;
+
+function scheduleCanvasReset() {
+  clearTimeout(resetTimeout);
+  resetTimeout = setTimeout(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }, 1000); // 1000 ms = 1 second
 }
 
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
 
-let drawing = false;
-
 canvas.addEventListener('pointerdown', (e) => {
   drawing = true;
   ctx.beginPath();
   ctx.moveTo(e.offsetX, e.offsetY);
+  scheduleCanvasReset();
 });
 
 canvas.addEventListener('pointermove', (e) => {
@@ -25,6 +36,8 @@ canvas.addEventListener('pointermove', (e) => {
   ctx.lineCap = 'round';
   ctx.lineJoin = 'round';
   ctx.stroke();
+  ctx.fillStyle = "orange";
+  scheduleCanvasReset();
 });
 
 canvas.addEventListener('pointerup', () => {
